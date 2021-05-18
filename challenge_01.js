01c3fe8e1fd822b5ff4866b70212eb9c4c023d97
Luqiiy <a.schlotte97@gmail.com>
2021-05-18 16:15:42 +0200

    challenge_01 bearbeitet+lösung

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,30 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 ‘Die Summe der Zahlen ist: summe ‘
 */
+/*
+let number1, number2, number3;
+number1 = prompt("Gib hier deine Zahl ein");
+number2 = prompt("Gib hier eine weitere Zahl ein!");
+
+
+number3 = +number1 + +number2;
+
+
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+
+console.log("Die Summe deiner Addition is : " + number3 );
+*/
+
+let num1 = parseInt(prompt("gib deine Zahl ein "));
+let num2 = parseInt(prompt("gib deine zweite Zahl ein"));
+
+//addition der zwei zahlen
+
+let sum = num1 + num2;
+
+console.log("die Summe von " +num1+ " und " +num2+ " ist gleich " +sum);
+
+
+
