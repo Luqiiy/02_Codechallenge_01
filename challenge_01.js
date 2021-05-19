5f4bc0ec971b02d959736dda8d332173bf190967
Luqiiy <a.schlotte97@gmail.com>
2021-05-19 09:09:56 +0200

    weiterelösungen +

--- challenge_01.js
+++ challenge_01.js
@@ -23,7 +23,8 @@ console.log(typeof number3);
 
 console.log("Die Summe deiner Addition is : " + number3 );
 */
-
+// meine lösung
+/*
 let num1 = parseInt(prompt("gib deine Zahl ein "));
 let num2 = parseInt(prompt("gib deine zweite Zahl ein"));
 
@@ -32,6 +33,79 @@ let num2 = parseInt(prompt("gib deine zweite Zahl ein"));
 let sum = num1 + num2;
 
 console.log("die Summe von " +num1+ " und " +num2+ " ist gleich " +sum);
+*/
+
+/*
+//vorschau dozent 
+// nicht richtige rechnnung
+let a = prompt("bitte zahl eingeben");
+let b = prompt("bitte eine zweite zahl eingeben");
+
+console.log(typeof a);
+console.log(typeof b);
+
+let c = a + b;
+console.log(" die summer der zahlen ist : " + c);
+*/
+
+
+// lösung dozent 1
+/*
+// deklaration
+let ziffer1;
+let ziffer1, ziffer2, summe;
+
+
+//eingabe :: ziffern
+let a = prompt("bitte zahl eingeben");              //2
+let b = prompt("bitte eine zweite zahl eingeben");  //2
+
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// typkonvertierung | ziffern --> zahl ( string --> number)
+
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// berechnung :: summe
+summe = zahl1 + zahl2;
+console.log("die summe is : " + summe);
+*/
+
+
+// **************Variante 2
+/*
+let zahl1, zahl2, summe;
+zahl1 = parseInt(prompt("ziffer eingeben"));
+zahl2 = parseInt(prompt("weiter zahl eingeben"));
+summe = zahl2 + zahl2;
+console.log(" die summer der zahlen ist : " + summe);
+*/
+
+
+// ************variante 3  
+//kompakt, aber schlecht lesbar
+/*
+console.log
+(
+    "die summe ist :" +
+    (
+        parseInt(prompt("bitte zahl 1 eingeben")) +
+        parseInt(prompt("bitte zahl 2 eingeben"))
+    )
+);
+*/
+
+
+
+
+
+
+
+
 
 
 
